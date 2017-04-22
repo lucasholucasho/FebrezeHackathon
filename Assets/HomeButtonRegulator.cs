ab1087c8ef31657866d9ce671af597d9357693d7
lucasholucasho <lucasho2340@gmail.com>
2017-04-22 03:18:24 -0700

    New UI flow for beach scene
    The “go home” button is hidden unless the user is looking at it.
    Clicking that button takes the user home. The only way to go home is to
    click on that button - clicking anywhere in the scene no longer takes
    the user home

---
+++ Assets/HomeButtonRegulator.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class HomeButtonRegulator : MonoBehaviour {
+	GameObject button;
+	Image buttonImage;
+
+	// Use this for initialization
+	void Start () {
+		button = GameObject.Find ("HomeButton");
+		buttonImage = button.GetComponent<Image> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ShowHomeButton() {
+		buttonImage.enabled = true;
+	}
+
+	public void HideHomeButton() {
+		buttonImage.enabled = false;
+	}
+
+	public void GoHome() {
+		AudioSource[] sounds = GetComponents<AudioSource> ();
+		AudioSource stopEchoSound = sounds[0];
+		stopEchoSound.Play();
+		RestoreDevicesAndLevel ();
+	}
+
+	public void RestoreDevicesAndLevel() {
+		StartCoroutine (RestorationWrapper());
+	}
+
+	private IEnumerator RestorationWrapper() {
+		yield return StartCoroutine(RestoreNest ());
+		SceneManager.LoadScene(0);
+	}
+
+	IEnumerator RestoreNest() {
+		return DevicesHelper.ChangeNestHelper ("60", "\"cool\"");
+	}
+}
